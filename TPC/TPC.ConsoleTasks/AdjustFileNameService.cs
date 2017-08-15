3364903d152f4695185183273470d11338463f66
pete <pokrovskiy.p@gmail.com>
2017-08-15 17:00:16 +0300

    Контент по семье года 2016 + консольное приложение для переименованя фото

---
+++ TPC/TPC.ConsoleTasks/AdjustFileNameService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPC.ConsoleTasks
+{
+    class AdjustFileNameService
+    {
+        public void AdjustName(string directoryPath, string nameMask)
+        {
+
+            if (!Directory.Exists(directoryPath))
+                throw new Exception($"Не существует директория {directoryPath}");
+
+            var directory = new DirectoryInfo(directoryPath);
+
+
+            var files = directory.GetFiles();
+
+
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+
+                System.Console.WriteLine(file.FullName);
+
+
+                file.MoveTo(file.Directory.FullName + "\\" + string.Format(nameMask, i));
+
+                //File.Move(file.FullName, Path.Combine(file.Directory.FullName, string.Format(nameMask, i)));
+
+                //file.Move(file.Name, string.Format(nameMask, i));
+                //File.Move(Path.Combine(directoryPath, file.Name), Path.Combine(directoryPath, string.Format(nameMask, i)));
+
+            }
+
+        }
+    }
+}
---
+++ TPC/TPC.ConsoleTasks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPC.ConsoleTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string originalNameMask = "family-of-year-2016-{0}.jpg";
+            const string thumbnailNameMask = "family-of-year-2016-thumb-{0}.jpg";
+
+            AdjustFileNameService service = new AdjustFileNameService();
+            service.AdjustName("C:\\output-thumbnails", thumbnailNameMask);
+
+
+            Console.ReadLine();
+        }
+    }
+}
